1ee2cfa4da5c996bb70c045b996ad3d1e90d14df
sabina381 <sabina2378@ewhain.net>
2024-11-23 18:50:58 +0900

    main 코드 작성
    - history를 파일로 저장하는 과정 예시

--- self_play.py
+++ self_play.py
@@ -1,6 +1,7 @@
 # import
 import numpy as np
 import copy
+import pickle
 
 from tictactoe_env import Environment
 from ResNet import ResidualBlock, Net
@@ -11,13 +12,7 @@ from mcts import Mcts
 SP_GAME_COUNT = 500  # 셀프 플레이를 수행할 게임 수(오리지널: 25,000)
 SP_TEMPERATURE = 1.0  # 볼츠만 분포의 온도 파라미터
 
-state_size = (3,3)
-reward_dict = {'win':1, 'lose':-1, 'draw':0, 'progress':0}
-env = Environment(state_size, reward_dict)
-
 CONV_UNITS = 64
-model = Net(state_size, env.num_actions, CONV_UNITS)
-
 
 # 1번의 게임 play 함수
 def play_one_game(model):
@@ -39,7 +34,7 @@ def play_one_game(model):
 
 
 # self play 함수
-def self_play():
+def self_play(model):
     env = copy.deepcopy(env)
     data = []
 
@@ -48,3 +43,29 @@ def self_play():
         data.extend(history)
 
     return data
+
+
+#############################
+# 동작 확인 및 저장 예시
+if __name__ == '__main__':
+    file_name = "model1"
+
+    state_size = (3,3)
+    env = Environment(state_size)
+    model = Net(state_size, env.num_actions, CONV_UNITS)
+
+    # 원래 history 불러오기
+    try:
+        with open(f'{file_name}_history.pkl', 'rb') as f:
+            origin_data = pickle.load(f)
+    except FileNotFoundError:
+        origin_data = [] # 파일이 비어있는 경우 빈 리스트 생성
+
+    # self-play로 새롭게 얻은 history
+    data = self_play(model)
+    # 원래 history에 추가
+    origin_data.extend(data)
+
+    # 파일에 저장
+    with open(f'{file_name}_history.pkl', 'wb') as f:
+            pickle.dump(origin_data, f)
