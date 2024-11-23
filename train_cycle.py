19d4ec0dd2011ed264b82eff4747177a3f0e0adc
sabina381 <sabina2378@ewhain.net>
2024-11-23 18:52:33 +0900

    초안 완성

--- train_cycle.py
+++ train_cycle.py
@@ -1,1 +1,27 @@
-# network train하는 cycle 함수 (main에 합치는 것도 나쁘지 않다고 생각함. 고민)
+# import
+from ResNet import Net
+from tictactoe_env import Environment
+from self_play import self_play
+from train_network import train_network
+from evaluate_network import evaluate_network
+from evaluate_best_player import evaluate_best_player
+
+# parameter
+TRAIN_NUM = 10
+
+state_size = (3,3)
+reward_dict = {'win':1, 'lose':-1, 'draw':0, 'progress':0}
+env = Environment(state_size, reward_dict)
+
+CONV_UNITS = 64
+model = Net(state_size, env.num_actions, CONV_UNITS)
+
+
+# network train cycle
+for i in range(TRAIN_NUM):
+    self_play()
+    train_network()
+    update_best_player = evaluate_network()
+
+    if update_best_player:
+        evaluate_best_player()
